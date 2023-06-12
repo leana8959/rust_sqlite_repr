710316d011071bdade435fad80dae904218bfea0
Leana <leana.jiang@icloud.com>
2023-06-12 15:22:11 +0000

    simplification

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM rust:1.70
+WORKDIR /usr/src/myapp/
+COPY . .
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use sqlite::Connection;
+fn main() {}
 
-fn main() {
+#[test]
+fn basic_test() {
+    // Example code from SQLite
+    // This would always work
+    let connection = sqlite::open(":memory:").unwrap();
+
+    let query = "
+        CREATE TABLE users (name TEXT, age INTEGER);
+        INSERT INTO users VALUES ('Alice', 42);
+        INSERT INTO users VALUES ('Bob', 69);
+    ";
+    connection.execute(query).unwrap();
+
+    let query = "SELECT * FROM users WHERE age > ?";
+    let mut statement = connection.prepare(query).unwrap();
+    statement.bind((1, 50)).unwrap();
+
+    statement.next().unwrap();
+    assert_eq!("Bob", statement.read::<String, _>("name").unwrap());
+    assert_eq!(69, statement.read::<i64, _>("age").unwrap());
+}
+
+#[test]
+fn upsert_test() {
+    use sqlite::Connection;
+
     let conn = Connection::open(":memory:").unwrap();
 
     conn.execute("CREATE TABLE users(name TEXT, age INTEGER, PRIMARY KEY (name))")
@@ -31,6 +56,6 @@ fn main() {
 
     let age = stmt.read::<i64, _>("age").unwrap();
 
-    println!("Hello, world!");
-    println!("age: {}", age);
+    println!("age = {}", age);
+    assert_eq!(50, age);
 }
